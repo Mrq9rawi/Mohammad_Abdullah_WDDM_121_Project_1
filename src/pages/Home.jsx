b1c0de6030e2b35ce188d5ad36f48b6f879b1bd1
Mrq9rawi <moh.abdullah02@gmail.com>
2022-06-14 20:24:26 -0400

    added a slide show

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import Container from "react-bootstrap/Container";
 import ServicesCard from "../components/ServicesCard";
 import Row from "react-bootstrap/Row";
 import servicesCards from "../servicesCards.json";
+import Carousel from "react-bootstrap/Carousel";
+import slideOneImage from "../img/slide-one.jpg";
+import slideTwoImage from "../img/slide-two.jpg";
+import slideThreeImage from "../img/slide-three.jpg";
 
 export default function Home() {
 	const cardsComp = servicesCards.map((card) => {
@@ -15,9 +19,36 @@ export default function Home() {
 				<Container>
 					<h1 className="fs-4 p-relative">
 						Hello, I'm
-						<span className="my-name fs-1 d-block"> Mohammad Abdullah </span>
+						<span className="my-name fs-1 d-block fw-bolder "> Mohammad Abdullah </span>
 						<span className="my-title fs-3">Front End Developer</span>
 					</h1>
+				</Container>
+			</section>
+			<section className="slide-show" id="slide-show">
+				<Container>
+					<Carousel>
+						<Carousel.Item interval={1000}>
+							<img className="d-block w-100" src={slideOneImage} alt="First slide" />
+							<Carousel.Caption>
+								<h3>First slide label</h3>
+								<p>text</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item interval={500}>
+							<img className="d-block w-100" src={slideTwoImage} alt="Second slide" />
+							<Carousel.Caption>
+								<h3>Second slide label</h3>
+								<p>text</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+						<Carousel.Item>
+							<img className="d-block w-100" src={slideThreeImage} alt="Third slide" />
+							<Carousel.Caption>
+								<h3>Third slide label</h3>
+								<p>text</p>
+							</Carousel.Caption>
+						</Carousel.Item>
+					</Carousel>
 				</Container>
 			</section>
 			<section>
