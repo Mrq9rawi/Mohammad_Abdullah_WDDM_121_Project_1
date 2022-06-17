29d01e004d12b6431d5dd9aa20bb607a29f46a1e
Mrq9rawi <moh.abdullah02@gmail.com>
2022-06-17 09:56:21 -0400

    fixed issue with all.min.js and testing gh pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import "./css/all.min.css";
-import "./js/all.min";
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.css';
